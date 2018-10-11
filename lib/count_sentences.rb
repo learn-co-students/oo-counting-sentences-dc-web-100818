bad8ddbd1c52388d3218e7ff3a39956bcdec2097
Drewisatlas <drewisatlas@gmail.com>
2018-10-11 11:02:15 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,28 @@ require 'pry'
 
 class String
 
+  def ending(punctuation)
+    if self.end_with?(punctuation)
+      true
+    else
+      false
+    end
+  end
+
   def sentence?
-    
+    ending(".")
   end
 
   def question?
-
+    ending("?")
   end
 
   def exclamation?
-
+    ending("!")
   end
 
   def count_sentences
-
+    array = self.split(/[?!.]/) #splitting by "?", "!", and "." (multiple delimiters)
+    array.delete_if {|string| string == ""}.count
   end
 end
