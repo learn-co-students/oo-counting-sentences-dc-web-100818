507cd0f2c6dd072f138a1b2da211f1bfa69da9b5
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-11 03:18:00 -0400

    OO Counting Sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    end
+    false
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    end 
+    false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if {|sentence| sentence == "" }
+    sentence_array.length
   end
 end
